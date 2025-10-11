3dfa889ac11f25c4438193243b0e444e077e137d
Isaul <tiradolisaul@gmail.com>
2025-10-11 10:59:53 -0700

    Agregar csrc/config.py-get_logger: funciÂón para obtener logger con formato y colores

--- src/config.py
+++ src/config.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+import logging
+import colorlog
 
 # Rutas base
 ROOT_DIR = Path(__file__).resolve().parent
@@ -10,3 +12,26 @@ INTERIM_DIR = DATA_DIR / "interim"
 def init_paths(): 
     for path in [DATA_DIR, RAW_DIR, PROCESSED_DIR, INTERIM_DIR]: 
         path.mkdir(parents=True, exist_ok=True)
+
+
+def get_logger(name):
+    handler = colorlog.StreamHandler()
+    handler.setFormatter(colorlog.ColoredFormatter(
+        "%(log_color)s%(asctime)s-%(levelname)s-%(name)s-%(message)s",
+        log_colors={
+            'DEBUG':    'cyan',
+            'INFO':     'green',
+            'WARNING':  'yellow',
+            'ERROR':    'red',
+            'CRITICAL': 'bold_red'
+        }
+    ))
+
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+
+    if not logger.handlers:
+        logger.addHandler(handler)
+
+    logger.propagate = False
+    return logger
