4308002cdc46a5253166cfc615f202f11af2d2a3
Isaul <tiradolisaul@gmail.com>
2025-11-02 13:34:46 -0700

    Modificar proceso de extracción VIALIDADES-OSM    - Nuevas rutas y formato de salida

--- src/extract_vialidades.py
+++ src/extract_vialidades.py
@@ -1,104 +1,112 @@
 """
 extract_vialidades.py
 
-...
+Descarga la red vial de Hermosillo, Sonora, desde OpenStreetMap (OSM) utilizando la librería OSMnx, 
+y exporta los datos de nodos y edges a formato Shapefile.
+Guarda los resultados limpios en data/processed
 """
 
-from config import RAW_DIR, ROOT_DIR, INTERIM_DIR, get_logger
-
+from config import ROOT_DIR, RAW_DIR, get_logger
 from datetime import datetime
+import warnings
 from pathlib import Path
-
 import osmnx as ox
-import geopandas as gpd
 
+warnings.filterwarnings('ignore')
 
 # Paths
-INTERIM_GEO_DIR = INTERIM_DIR / "geo"
-INTERIM_GEO_DIR.mkdir(exist_ok=True)
+RAW_VIALIDADES_DIR = RAW_DIR / "vialidades"
+RAW_VIALIDADES_DIR.mkdir(exist_ok=True)
 
-CACHE_DIR = INTERIM_GEO_DIR / "cache"
+CACHE_DIR = RAW_VIALIDADES_DIR / "cache_osmnx"
 CACHE_DIR.mkdir(exist_ok=True)
-
 
 # Logger
 logger = get_logger(Path(__file__).name)
 
-
 # Configuración de OSMnx
 ox.settings.use_cache = True
 ox.settings.cache_folder = str(CACHE_DIR)
-ox.settings.log_console = False  # Cambir a False
+ox.settings.log_console = False
 
 
-def generate_documentation_vialidades():
-    doc_content = f"""
-# DESCRIPCIÓN DE FUENTES DE DATOS - VIALIDADES HMO
+def download_hmo_roads(network_type="drive", simplify=False):
+    place = 'Hermosillo, Sonora, México'
 
-Fuente: OpenStreetMap (vía OSMnx)
-- Nombre de la Fuente: Red vial de Hermosillo
-- Herramienta de Extracción: OSMnx (https://osmnx.readthedocs.io/)
-- Lugar: Hermosillo, Sonora, México
-- Tipo de Red: drive
-- Fecha de Extracción: {datetime.now().strftime("%Y-%m-%d %H:%M:%S")}
-- Descripción de los Datos:
-    Red vial obtenida desde OpenStreetMap, que incluye nodos (intersecciones)
-    y edges (segmentos de calle) para análisis de conectividad y movilidad urbana.
-- Archivos Generados:
-    - hermosillo_nodes.shp
-    - hermosillo_edges.shp
-- Formato de los Datos: Shapefile (GeoDataFrame)
-"""
-    doc_path = RAW_DIR / "documentacion_vialidades.txt"
-    with open(doc_path, "w", encoding="utf-8") as f:
-        f.write(doc_content)
-    logger.info(f"Documentación de fuentes generada en: {doc_path}")
-
-
-def download_hmo_roads(network_type="drive", simplify=True):
-    place = "Hermosillo, Sonora, México"
-    logger.info(f"Descargando red vial de {place} (tipo={network_type})...")
+    start = datetime.now()
+    logger.info(f'Descargando red vial de {place} (tipo={network_type})...')
 
     G = ox.graph_from_place(place, network_type=network_type, simplify=simplify)
 
-    logger.info("Descarga completada")
+    end = datetime.now()
+    elapsed = (end - start).total_seconds()
+
+    logger.info(f'Descarga de red vial de {place} (tipo={network_type}) finalizada en {elapsed:.2f} s')
     return G
 
-def export_graph_to_shapefiles(G, output_dir=INTERIM_GEO_DIR): 
-    logger.info("Convirtiendo grafo a GeoDataFrames...")
+
+def export_graph_to_shapefiles(G, output_dir=RAW_VIALIDADES_DIR):
+    logger.info('Convirtiendo grafo a shapefiles...')
     nodes, edges = ox.graph_to_gdfs(G)
 
-    edges_path = output_dir / "hermosillo_edges.shp"
-    nodes_path = output_dir / "hermosillo_nodes.shp"
+    edges_dir = output_dir / "edges"
+    nodes_dir = output_dir / "nodes"
+    edges_dir.mkdir(parents=True, exist_ok=True)
+    nodes_dir.mkdir(parents=True, exist_ok=True)
 
-    logger.info(f"Exportando edges -> {edges_path.relative_to(ROOT_DIR)}")
+    # Rutas de salida
+    edges_path = edges_dir / "hermosillo_edges.shp"
+    nodes_path = nodes_dir / "hermosillo_nodes.shp"
+
+    # Exportar shapefiles
+    logger.info(f'Exportando edges en: {edges_path.relative_to(ROOT_DIR)}')
     edges.to_file(edges_path)
 
-    logger.info(f"Exportando nodes -> {nodes_path.relative_to(ROOT_DIR)}")
+    logger.info(f'Exportando nodes en: {nodes_path.relative_to(ROOT_DIR)}')
     nodes.to_file(nodes_path)
 
     return nodes_path, edges_path
 
 
-def process_extraction_roads_hmo(): 
-    start = datetime.now()
-    logger.info("Inicia proceso de extracción de vialidades: ")
+def export_graph_to_geojson(G, output_dir=RAW_VIALIDADES_DIR):
 
-    G = download_hmo_roads()
-    nodes_path, edges_path, = export_graph_to_shapefiles(G)
+    logger.info('Convirtiendo grafo a GeoJSON...')
+    nodes, edges = ox.graph_to_gdfs(G)
 
-    end = datetime.now()
-    elapsed = (end - start).total_seconds()
-    logger.info(f"Proceso finalizado en {elapsed:.2f} s")
+    # Crear directorios
+    edges_dir = output_dir / "edges"
+    nodes_dir = output_dir / "nodes"
+    edges_dir.mkdir(parents=True, exist_ok=True)
+    nodes_dir.mkdir(parents=True, exist_ok=True)
 
-    generate_documentation_vialidades()
+    # Rutas de salida
+    edges_path = edges_dir / "hermosillo_edges.geojson"
+    nodes_path = nodes_dir / "hermosillo_nodes.geojson"
 
-    logger.info("Archivos generados en:")
-    for p in INTERIM_GEO_DIR.glob("*.shp"):
-        print(f" - {p.relative_to(ROOT_DIR)}")
-        
+    # Exportar GeoJSON
+    logger.info(f'Exportando edges en: {edges_path.relative_to(ROOT_DIR)}')
+    edges.to_file(edges_path, driver='GeoJSON')
+
+    logger.info(f'Exportando nodes en: {nodes_path.relative_to(ROOT_DIR)}')
+    nodes.to_file(nodes_path, driver='GeoJSON')
+
     return nodes_path, edges_path
 
 
+def process_extraction_vialidades(): 
+    start = datetime.now()
+    logger.info('Inicia proceso de extracción VIALIDADES-OSM: ')
+
+    G = download_hmo_roads()
+    nodes_path, edges_path, = export_graph_to_shapefiles(G)
+    nodes_geojson_path, edges_geojson_path = export_graph_to_geojson(G)
+
+    end = datetime.now()
+    elapsed = (end - start).total_seconds()
+    logger.info(f'Proceso de extracción VIALIDADES-OSM finalizado en {elapsed:.2f} s')
+        
+    return nodes_path, edges_path, nodes_geojson_path, edges_geojson_path
+
+
 if __name__ == "__main__":
-    process_extraction_roads_hmo()
+    process_extraction_vialidades()
